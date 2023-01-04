6fe2b2dec12d1d079392d6071a358c027fb5ede0
nicolas <nicaso3006@gmail.com>
2023-01-03 22:16:52 -0400

    new changes

--- src/app/app.js
+++ src/app/app.js
@@ -16,12 +16,14 @@ export const app = (elementHtml) => {
     const $cards       = document.querySelector('#cards');
     const $btn_previus = document.querySelector('#btnAnterior');
     const $btn_next    = document.querySelector('#btnSiguiente');
+
     
     getApi($cards, pagesNumber);
-
     
     $btn_previus.addEventListener('click', () => {
         
+        if( pagesNumber === 1 ) return
+
         pagesNumber = pagesNumber - 1;
     
         $cards.innerHTML = '';
@@ -30,8 +32,10 @@ export const app = (elementHtml) => {
     
     $btn_next.addEventListener('click', () => {
         
+        if( pagesNumber === 36520) return
+
         pagesNumber = pagesNumber + 1;
-    
+
         $cards.innerHTML = '';
         getApi($cards, pagesNumber)
     })
--- src/moviedb/movie-db.js
+++ src/moviedb/movie-db.js
@@ -9,30 +9,39 @@
  */
 const getApi = async( element, pagesNumber ) => {
 
+    try {
+        const api  = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=908e7d342facb48032f42b20340155b8&language=en-US&page=${pagesNumber}`);
 
-    const api  = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=908e7d342facb48032f42b20340155b8&language=en-US&page=${pagesNumber}`);
-    const data = await api.json();
+        if( api.status === 200 ){
+            
+            const data = await api.json();
+                const movies = data.results.forEach( movie => {
+                    
+                    const template = `
+                    <div class="card-movie">
+                        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
+                        <div class="card-title">
+                        <h3>${movie.title}</h3>
+                        </div>
+                    </div>
+                    `;
+            
+                    element.innerHTML += ''
+                    element.innerHTML += template
+                })
+            
+                console.log(data);
+                return movies;
 
-    
-    const movies = data.results.forEach( movie => {
+        } else if( api.status === 401 || api.status === 404 ) return
         
-        const template = `
-        <div class="card-movie">
-            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
-            <div class="card-title">
-            <h3>${movie.title}</h3>
-            </div>
-        </div>
-        `;
 
-        element.innerHTML += ''
-        element.innerHTML += template
-    })
+    } catch (error) {
+        console.log(error);
+    }
 
-    console.log(data);
-
+    return
     
-    return movies;
 }
 
 
